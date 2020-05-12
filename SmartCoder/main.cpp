bb1b98709f9101872cacc479e9f02b8dd9a837ea
Administrator <christzhangowner@gmail.com>
2020-05-13 01:30:36 +0800

    20200513

--- SmartCoder/main.cpp
+++ SmartCoder/main.cpp
@@ -1,10 +1,25 @@
 #include "smartcoder.h"
 #include <QtWidgets/QApplication>
+#include <QSplashScreen>
+#include <QPropertyAnimation>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+
+	QSplashScreen* splash = new QSplashScreen;
+	splash->setPixmap(QPixmap(":/icons/Resources/icons/icon11.png"));
+	splash->show();
 	SmartCoder w;
+	QPropertyAnimation* animation = new QPropertyAnimation(&w, "windowOpacity");
+	animation->setDuration(500);
+	animation->setStartValue(0);
+	animation->setEndValue(1);
+	animation->start();
 	w.show();
+	splash->finish(&w);
+	delete splash;
+
 	return a.exec();
 }
